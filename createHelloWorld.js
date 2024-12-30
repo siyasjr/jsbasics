404bac57ded317911ff5d1927375e6b99449e873
siyasjr <siyasjr1@gmail.com>
2024-12-30 23:26:18 +0530

    This function returns a function which returns 'Hello World'

---
+++ createHelloWorld.js
@@ -0,0 +1,9 @@
+
+function createHelloWorld(){
+    function HelloWorld(){
+        const str = "Hello World";
+        return str;
+    }
+    return HelloWorld;
+}
+console.log(createHelloWorld());
