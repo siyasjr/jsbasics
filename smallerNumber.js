198e0334d13a819cca7905f26b27ddc65992a076
AJMAL SIYAS <65674473+siyasjr@users.noreply.github.com>
2025-01-01 23:34:16 +0530

    This function will find the smaller number of two given numbers

--- smallerNumber.js
+++ smallerNumber.js
@@ -1,4 +1,4 @@
-// This function will find the smaller number of two given numbers
+//  This function will find the smaller number of two given numbers
 
 function smallerNumber(num1, num2){
     if(num1 > num2){ // greater than conditional 
