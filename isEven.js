582da2840240552554056dfef3dd3edb5921796e
siyasjr <siyasjr1@gmail.com>
2025-01-01 23:15:36 +0530

    This code returns true if thegiven integer is even and false otherwise

---
+++ isEven.js
@@ -0,0 +1,10 @@
+/* 
+    Given an integer value num determine if it is even.
+    If even,return true.Otherwise return false . */
+
+    function isEven(num){
+        if(num % 2 === 0){
+            return true;
+        }
+        return false;
+    }
