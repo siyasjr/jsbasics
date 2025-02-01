32e7083d93c23d8f80b2132e29b9563d3ae4bbe8
siyasjr <siyasjr1@gmail.com>
2025-02-01 23:59:24 +0530

    Learned Array.prototype.sort

---
+++ sortStudents.js
@@ -0,0 +1,26 @@
+/* Given an array of students, sort first by graduated then by grade like in the example above.
+Each object in the students array will have properties id, graduated and grade */ 
+
+function sortStudents(students) {
+    return students.sort( (a,b) => {
+    if(a.graduated && !b.graduated){
+        return -1;
+    }
+    if(b.graduated && !a.graduated){
+            return 1;
+    }
+        if ((a.graduated === b.graduated)){
+            if (a.grade > b.grade) {
+                return -1;
+            }
+            if (b.grade > a.grade) {
+                return 1;
+            };
+        }
+
+    });
+
+    
+}
+
+module.exports = sortStudents;
