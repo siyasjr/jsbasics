2c34abc66470d4a28091fec750368a7501246f42
siyasjr <siyasjr1@gmail.com>
2025-02-10 23:32:36 +0530

    Initialize allUnique function with reduce; implementation pending.

---
+++ arrayReduceIsUnique.js
@@ -0,0 +1,11 @@
+// numbers is an array full of integers
+// let's figure if all the numbers are unique
+// i.e. [1,2,3,1] => false
+// [1,2,3] => true
+function allUnique(numbers) {
+    return numbers.reduce((accumulator, currentValue, index) => {
+        
+    }, true);
+}
+
+module.exports = allUnique;
