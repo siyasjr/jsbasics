30b846595d8b8099c1ea0957a311f899d98014a5
siyasjr <siyasjr1@gmail.com>
2025-02-12 23:34:51 +0530

    Add throwError function to throw a custom error message

---
+++ throwError.js
@@ -0,0 +1,6 @@
+// This function throws an Error and the Error message is written in the message section
+function throwError() {
+    throw new Error("Bad, no good!");
+}
+
+module.exports = throwError;
