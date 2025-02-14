f0d9ed60a1c5b312b737ad9e3b6fcdbd907007d2
siyasjr <siyasjr1@gmail.com>
2025-02-14 23:59:02 +0530

    Implement exclusive divisibility check for 3 and 5

---
+++ exclusiveDivisibilityCheck.js
@@ -0,0 +1,9 @@
+/* function eitherNotBoth that takes in a number and returns true if the the number is 
+divisible by either 3 or 5, but not both. Return false otherwise.*/
+function eitherNotBoth(num){
+    if ((num % 3 === 0 && num % 5 !==0) || (num % 5 === 0 && num % 3 !==0) ){
+        return true;
+    }
+    return false;
+}
+module.exports = eitherNotBoth;
