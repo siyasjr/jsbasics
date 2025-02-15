4db64c8f98d81c41d9431dce8cf38adbf4ecadef
siyasjr <siyasjr1@gmail.com>
2025-02-15 23:02:10 +0530

    mplemented toJSON(obj) function using JSON.stringify()

---
+++ toJSON.js
@@ -0,0 +1,7 @@
+The toJSON function converts a JavaScript object into a JSON (JavaScript Object Notation) string.
+
+function toJSON(obj) {
+    return JSON.stringify(obj);
+}
+
+module.exports = toJSON;
