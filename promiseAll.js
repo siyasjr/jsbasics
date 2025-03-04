63da281023422233928e6b7b3641df9141bbccb7
siyasjr <siyasjr1@gmail.com>
2025-03-04 23:58:26 +0530

    Use Promise.all() to execute multiple promises concurrently, handling rejection scenarios.

---
+++ promiseAll.js
@@ -0,0 +1,15 @@
+const promise1 = new Promise((resolve, reject) =>{
+    //resolve(["js","java"]);
+    reject('API fetch failed!')
+
+})
+
+const promise2 = new Promise((resolve, reject) =>{
+    resolve(["react","angular"]);
+    //reject('API fetch failed!')
+})
+
+
+Promise.all([promise1,promise2])
+.then((res) => {console.log(JSON.stringify(res));})
+.catch((err) => {console.log(err);})
