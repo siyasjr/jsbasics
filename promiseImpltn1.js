a1b498e0003919ba964d6983b0ea0d13bcae2f19
siyasjr <siyasjr1@gmail.com>
2025-03-07 22:44:29 +0530

    Implement basic Pact class as a simplified Promise alternative

---
+++ promiseImpltn1.js
@@ -0,0 +1,18 @@
+//Simplified Promise Implementation (Pact Class)
+class Pact {
+    constructor(fn) {
+        this.resolve = (value) => {
+            this.thenFn(value);
+        }
+        this.reject = () => {
+            
+        }
+        fn(this.resolve, this.reject);
+    }
+    then(_then) {
+        this.thenFn = _then;
+    }
+    catch() {
+        
+    }
+}
